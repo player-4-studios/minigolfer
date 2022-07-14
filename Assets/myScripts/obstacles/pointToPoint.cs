07a318d512415f20f3b728546ed628389844156e
Churchy0429 <Dosen260@gmail.com>
2022-07-14 13:14:20 -0400

    Holes laid out
    Holes 1-18 laid out with start and end triggers + obstacles. Created obstacle scripts for rotation and point to point movement

---
+++ Assets/myScripts/obstacles/pointToPoint.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pointToPoint : MonoBehaviour
+{
+    [SerializeField] Transform[] points;
+    [SerializeField] int pointsIndex;
+    [SerializeField] float navDistance;
+    [SerializeField] float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, points[pointsIndex].position, speed * Time.deltaTime);
+        if(Vector3.Distance(transform.position, points[pointsIndex].position) <= navDistance)
+        {
+            nextPoint();
+        }
+    }
+
+    public void nextPoint()
+    {
+        if(pointsIndex >= points.Length -  1)
+        {
+            pointsIndex = 0;
+        }
+        else
+        {
+            pointsIndex++;
+        }
+    }
+}
