210c73af7d77553f1d52a7553237c08d4bd60af5
Churchy0429 <Dosen260@gmail.com>
2022-07-19 16:32:58 -0400

    Main menu created

---
+++ Assets/myScripts/managers/menuManagers/mainMenuManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class mainMenuManager : MonoBehaviour
+{
+    [SerializeField] GameObject mainPanel;
+    [SerializeField] GameObject coursePanel;
+    [SerializeField] GameObject creditsPanel;
+    [SerializeField] GameObject optionsPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainPanel.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    public void backToMain()
+    {
+        mainPanel.SetActive(true);
+    }
+    public void courseSelection()
+    {
+        mainPanel.SetActive(false);
+        coursePanel.SetActive(true);
+    }
+
+    public void toOptions()
+    {
+        mainPanel.SetActive(false);
+        optionsPanel.SetActive(true);
+    }
+
+    public void creditsPan()
+    {
+        mainPanel.SetActive(false);
+        creditsPanel.SetActive(true);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+    public void loadCourse(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
